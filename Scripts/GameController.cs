ef0265619fb801f98a67b8061bc0cc01ac5aa974
Janders1800 <zenit1800@gmail.com>
2018-07-04 23:47:52 +0200

    Creation of the Godot project

---
+++ Scripts/GameController.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public class GameController : Spatial
+{
+    // Member variables here, example:
+    // private int a = 2;
+    // private string b = "textvar";
+
+    public override void _Ready()
+    {
+        // Called every time the node is added to the scene.
+        // Initialization here
+        
+        GD.Print ("Hello World!");
+    }
+
+//    public override void _Process(float delta)
+//    {
+//        // Called every frame. Delta is time since last frame.
+//        // Update game logic here.
+//        
+//    }
+}
