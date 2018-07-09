a723ba23b2178d156ec45161bafab35fec8526a7
Janders1800 <zenit1800@gmail.com>
2018-07-09 03:22:05 +0200

    Basic FPS controller

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Godot-Tournament
 A fast paced FPS made in Godot Engine
+
+WASD to move arround
+Shift to walk
+F to swich betwen fly mode and normal mode
+
+Ctrl+R to reload scene
+
+If the slope is more than 45º angle the character will not jump
--- Scripts/Player/Player.cs
+++ Scripts/Player/Player.cs
@@ -9,6 +9,7 @@ public class Player : KinematicBody
     //Fly variables
     [Export] float flySpeed = 5f;
     [Export] float flyAcceleration = 2f;
+    bool flyMode = false;
     Vector3 flyVelocity;
 
     //Move variables
@@ -18,19 +19,27 @@ public class Player : KinematicBody
     [Export] float moveWalkSpeed = 2f;
     [Export] float moveAcceleration = 2f;
     [Export] float moveDeceleration = 4f;
+    bool isWalking = false;
+    Vector3 velocity;
     
     //Jump variables
     [Export] float jumpSpeed = 15f;
     bool canJump = false;
-
-    bool isWalking = false;
-    Vector3 velocity;
+    bool hasFloorContact = false;
+    
 
     //View variables
+    [Export] float maxAngleView = 90f;
+    [Export] float minAngleView = -90f;
     float cameraAngle = 0f;
     float headRelativeAngle = 0f;
+
+    //Slope variables
+    [Export] float maxSlopeAngle = 35f;
+
     Camera camera;
     Spatial head;
+    RayCast floorChecker;
     Vector3 direction;
 
     public override void _Ready()
@@ -41,7 +50,14 @@ public class Player : KinematicBody
 
     public override void _PhysicsProcess(float delta)
     {
-        MoveCharacter(delta);
+        if (flyMode)
+        {
+            MoveCharacterFly(delta);
+        }
+        else
+        {
+            MoveCharacter(delta);
+        }
     }
 
     public override void _Process(float delta)
@@ -59,21 +75,28 @@ public class Player : KinematicBody
     {
         direction = Vector3.Zero;
 
+        Basis headDirection = head.GetGlobalTransform().basis;
         Basis cameraDirection = camera.GetGlobalTransform().basis;
 
-        if (Input.IsActionPressed("move_forward")) direction -= cameraDirection.z;
-        if (Input.IsActionPressed("move_backward")) direction += cameraDirection.z;
-        if (Input.IsActionPressed("move_left")) direction -= cameraDirection.x;
-        if (Input.IsActionPressed("move_right")) direction += cameraDirection.x;
-        isWalking = Input.IsActionPressed("move_walk");
-        canJump = Input.IsActionJustPressed("jump");
+        if (flyMode)
+        {
+            CalculateFlyDirection(cameraDirection);
+        }
+        else
+        {
+            CalculateMoveDirection(cameraDirection, headDirection);
+        }
 
         direction = direction.Normalized();
+
+        isWalking = Input.IsActionPressed("move_walk");
+        canJump = Input.IsActionJustPressed("jump") && hasFloorContact;
+        if (Input.IsActionJustPressed("fly_mode")) flyMode = !flyMode;
     }
 
     void MoveCharacter(float delta)
     {
-        velocity.y += gravity * delta;
+        CalculateMoveToFloor(delta);
 
         Vector3 tempVelocity = velocity;
         tempVelocity.y = 0f;
@@ -98,7 +121,11 @@ public class Player : KinematicBody
         velocity.x = tempVelocity.x;
         velocity.z = tempVelocity.z;
 
-        if (canJump) velocity.y = jumpSpeed;
+        if (canJump)
+        {
+            velocity.y = jumpSpeed;
+            hasFloorContact = false;
+        }
 
         velocity = MoveAndSlide(velocity, Vector3.Up);
     }
@@ -117,7 +144,7 @@ public class Player : KinematicBody
         {
             headRelativeAngle = Mathf.Deg2Rad(-mouseMotion.Relative.x * mouseSensitivity);
 
-            cameraAngle = Mathf.Clamp(cameraAngle - mouseMotion.Relative.y * mouseSensitivity, -90f, 90f);
+            cameraAngle = Mathf.Clamp(cameraAngle - mouseMotion.Relative.y * mouseSensitivity, minAngleView, maxAngleView);
         }
     }
 
@@ -125,6 +152,7 @@ public class Player : KinematicBody
     {
         head = (Spatial) FindNode("Head");
         camera = (Camera) FindNode("Camera");
+        floorChecker = (RayCast) FindNode("FloorChecker");
     }
 
     void RotateView()
@@ -132,5 +160,44 @@ public class Player : KinematicBody
         head.RotateY(headRelativeAngle);
         camera.SetRotationDegrees(new Vector3(cameraAngle, 0f, 0f));
         headRelativeAngle = 0f;
+    }
+    
+    void CalculateFlyDirection(Basis cameraDirection)
+    {
+        if (Input.IsActionPressed("move_forward")) direction -= cameraDirection.z;
+        if (Input.IsActionPressed("move_backward")) direction += cameraDirection.z;
+        if (Input.IsActionPressed("move_left")) direction -= cameraDirection.x;
+        if (Input.IsActionPressed("move_right")) direction += cameraDirection.x;
+    }
+
+    void CalculateMoveDirection(Basis cameraDirection, Basis headDirection)
+    {
+        if (Input.IsActionPressed("move_forward")) direction -= headDirection.z;
+        if (Input.IsActionPressed("move_backward")) direction += headDirection.z;
+        if (Input.IsActionPressed("move_left")) direction -= cameraDirection.x;
+        if (Input.IsActionPressed("move_right")) direction += cameraDirection.x;
+    }
+
+    void CalculateMoveToFloor(float delta)
+    {
+        if (IsOnFloor())
+        {
+            hasFloorContact = true;
+
+            Vector3 floorNormal = floorChecker.GetCollisionNormal();
+            float floorAngle = Mathf.Rad2Deg(Mathf.Acos(floorNormal.Dot(Vector3.Up)));
+
+            if (floorAngle > maxSlopeAngle) velocity.y += gravity * delta;
+        }
+        else
+        {
+            if (!floorChecker.IsColliding())
+            {
+                velocity.y += gravity * delta;
+                hasFloorContact = false;
+            }
+        }
+
+        if (hasFloorContact && !IsOnFloor()) MoveAndCollide(Vector3.Down);
     }
 }
